d8e08996f1d42f8738d8844e3a2d0c69c4192f59
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-08-01 10:52:04 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,11 +9,7 @@ def view(requests):
         if ' ' in job:
             requirements_skills = {'Текст не може бути пустим'} 
         elif job != '' and type(job) == str:
-<<<<<<< HEAD
             parser = parserhh.ParserHH()
             requirements_skills = parser.requirements_skills(job)
-=======
-            requirements_skills = parserhh.requirements_skills(job)
->>>>>>> b9f5dcff4b361722d80e61da84d85811b9864a6c
 
     return render(requests, 'main.html', {'skills': list(requirements_skills)})
