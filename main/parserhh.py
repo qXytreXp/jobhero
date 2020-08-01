00b22018f03a7326c885abc2a4c24ec33e3b4996
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-08-01 10:53:57 +0300

    Update parserhh.py

--- main/parserhh.py
+++ main/parserhh.py
@@ -198,33 +198,7 @@ class ParserHH:
     def thread_search_requirements_six(self, links, job):
         links = links
         arr_skills = []
-=======
-
-
-def search_links(job):
-    url = 'https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&fromSearch=true&text='+ job +'&from=suggest_post'
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
-
-    html = requests.get(url, headers={'User-agent':user_agent}).content
-    soup = BeautifulSoup(html, 'html.parser')
-    link = soup.find_all('a', class_='bloko-link HH-LinkModifier')
-
-    arr_links = []
-
-    for links in link:
-        if len(arr_links) == 20:
-            break
-
-        arr_links.append(links['href'])
-
-    return arr_links
-
-
-def requirements(job):
-        arr_skills = []
-        links = search_links(job)
->>>>>>> b9f5dcff4b361722d80e61da84d85811b9864a6c
-
+        
         for link in links:
             try:
                 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
@@ -240,7 +214,6 @@ def requirements(job):
             except AttributeError:
                 pass
 
-<<<<<<< HEAD
         self.res_thread_search_requirements_six = arr_skills
 
 
@@ -260,25 +233,4 @@ def requirements(job):
                 res.append(count_skill)
 
         return set(res)
-=======
-        return arr_skills
-
-
-def requirements_skills(job):
-    res = []
-    count_skills = []
-    arr_skills = requirements(job)
-
-    c = collections.Counter(arr_skills)
-
-    for arr_skill in arr_skills:
-        count_skills.append(c[arr_skill])
-
-    for count_skill in arr_skills:
-        if c[count_skill] > max(count_skills) // c[count_skill]:
-            res.append(count_skill)
-
-    return set(res)
->>>>>>> b9f5dcff4b361722d80e61da84d85811b9864a6c
-
 
